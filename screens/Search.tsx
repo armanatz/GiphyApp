73ba2b8e1825299348d8e05bdbed3cd610d402ad
Arman Attarzadeh <3622268+armanatz@users.noreply.github.com>
2023-08-28 22:10:45 +0800

    Focus search input if navigating from home screen

--- screens/Search.tsx
+++ screens/Search.tsx
@@ -1,5 +1,7 @@
+import { useRef, useEffect } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import type { TextInput } from 'react-native';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 import SearchBar from '../components/SearchBar';
@@ -14,9 +16,24 @@ const styles = StyleSheet.create({
 });
 
 export default function Search({ navigation }: SearchProps) {
+  const searchInputRef = useRef<TextInput | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener('transitionEnd', () => {
+      if (searchInputRef.current !== null) {
+        searchInputRef.current.focus();
+      }
+    });
+
+    return unsubscribe;
+  }, [navigation]);
+
   return (
     <SafeAreaView>
-      <SearchBar onCancelSearch={() => navigation.goBack()} />
+      <SearchBar
+        ref={searchInputRef}
+        onCancelSearch={() => navigation.goBack()}
+      />
       <View style={styles.content}>
         <Text>Search results:</Text>
       </View>
