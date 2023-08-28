a1db145909b1782ce97356fd91adfccb9ec74d38
Arman Attarzadeh <3622268+armanatz@users.noreply.github.com>
2023-08-28 19:56:19 +0800

    Set up common screen styles and options

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet } from 'react-native';
 
 import Home from './screens/Home';
 import Search from './screens/Search';
@@ -11,15 +12,45 @@ export type RootStackParams = {
   ViewGIF: undefined;
 };
 
+const styles = StyleSheet.create({
+  container: {
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+});
+
+const commonStackScreenOpts = {
+  headerShown: false,
+  contentStyle: styles.container,
+};
+
 export default function App() {
   const Stack = createNativeStackNavigator<RootStackParams>();
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Search" component={Search} />
-        <Stack.Screen name="ViewGIF" component={ViewGIF} />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{
+            ...commonStackScreenOpts,
+          }}
+        />
+        <Stack.Screen
+          name="Search"
+          component={Search}
+          options={{
+            ...commonStackScreenOpts,
+          }}
+        />
+        <Stack.Screen
+          name="ViewGIF"
+          component={ViewGIF}
+          options={{
+            ...commonStackScreenOpts,
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
