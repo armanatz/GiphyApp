6122961effed47ea6399674c60c97c8af0c0fd70
Arman Attarzadeh <3622268+armanatz@users.noreply.github.com>
2023-09-02 18:22:06 +0800

    Implement random GIF query into Home screen

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,8 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { useState } from 'react';
 
-import { SearchBar } from '../components';
+import { GIFDetails, GIFDetailsSkeleton, SearchBar } from '../components';
+
+import { useRandomGIF } from '../hooks/queries';
 
 import type { RootStackParams } from '../@types/globals';
 
@@ -15,6 +18,17 @@ const styles = StyleSheet.create({
 });
 
 export default function Home({ navigation }: HomeProps) {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const { data: randomGIF, isFetching: randomGIFIsFetching } = useRandomGIF({
+    refetchInterval: data => {
+      if (isImageLoaded && data) {
+        return 10000;
+      }
+      return 0;
+    },
+  });
+
   return (
     <SafeAreaView>
       <SearchBar
@@ -23,6 +37,22 @@ export default function Home({ navigation }: HomeProps) {
       />
       <View style={styles.content}>
         <Text>Random selected GIF:</Text>
+        {randomGIFIsFetching ? (
+          <GIFDetailsSkeleton />
+        ) : (
+          <GIFDetails
+            source={
+              randomGIF?.data.images.original.webp ||
+              randomGIF?.data.images.original.url
+            }
+            title={randomGIF?.data.title}
+            shortUrl={randomGIF?.data.bitly_url || randomGIF?.data.url}
+            rating={randomGIF?.data.rating}
+            onImageLoadingStatusChange={imageLoadingStatus =>
+              setIsImageLoaded(imageLoadingStatus === 'success')
+            }
+          />
+        )}
       </View>
     </SafeAreaView>
   );
