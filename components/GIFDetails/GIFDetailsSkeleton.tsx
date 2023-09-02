f28467ea111eaeb7bd33d2098e1d44667568ad50
Arman Attarzadeh <3622268+armanatz@users.noreply.github.com>
2023-09-02 18:13:11 +0800

    Create GIF details skeleton placeholder component

---
+++ components/GIFDetails/GIFDetailsSkeleton.tsx
@@ -0,0 +1,38 @@
+import { View, StyleSheet, Dimensions } from 'react-native';
+
+import SkeletonPlaceholder from '../SkeletonPlaceholder';
+
+import commonStyles from './commonStyles';
+
+const styles = StyleSheet.create({
+  titleText: {
+    width: Dimensions.get('screen').width * 0.5,
+    height: 16,
+    marginBottom: 5,
+  },
+  urlText: {
+    width: Dimensions.get('screen').width * 0.6,
+    height: 16,
+  },
+});
+
+export default function GIFDetailsSkeleton() {
+  return (
+    <>
+      <View>
+        <SkeletonPlaceholder>
+          <SkeletonPlaceholder.Item style={commonStyles.imageContainer} />
+        </SkeletonPlaceholder>
+      </View>
+      <View style={commonStyles.descriptionContainer}>
+        <SkeletonPlaceholder>
+          <SkeletonPlaceholder.Item style={styles.titleText} />
+          <SkeletonPlaceholder.Item style={styles.urlText} />
+        </SkeletonPlaceholder>
+        <SkeletonPlaceholder>
+          <SkeletonPlaceholder.Item style={commonStyles.ratingCircle} />
+        </SkeletonPlaceholder>
+      </View>
+    </>
+  );
+}
--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,12 @@ import SearchBar from './SearchBar';
 import SearchResultList from './SearchResultList';
 import ImageWrapper from './ImageWrapper';
 import GIFDetails from './GIFDetails';
+import GIFDetailsSkeleton from './GIFDetails/GIFDetailsSkeleton';
 
-export { SearchBar, SearchResultList, ImageWrapper, GIFDetails };
+export {
+  SearchBar,
+  SearchResultList,
+  ImageWrapper,
+  GIFDetails,
+  GIFDetailsSkeleton,
+};
