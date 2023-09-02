f91f2c990ca361a7e7ed31bf4cb386bf5e64d862
Arman Attarzadeh <3622268+armanatz@users.noreply.github.com>
2023-09-02 21:15:38 +0800

    Implement GIFDetails into ViewGIF screen

--- screens/ViewGIF.tsx
+++ screens/ViewGIF.tsx
@@ -1,10 +1,25 @@
-import { Text } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import type { NativeStackScreenProps } from '@react-navigation/native-stack';
 
-export default function ViewGIF() {
+import { GIFDetails } from '../components';
+import type { RootStackParams } from '../@types/globals';
+
+type ViewGIFProps = NativeStackScreenProps<RootStackParams, 'ViewGIF'>;
+
+export default function ViewGIF({ route }: ViewGIFProps) {
+  const { source, title, shortUrl, rating } = route.params;
+
   return (
     <SafeAreaView>
-      <Text>GIF View</Text>
+      <View>
+        <GIFDetails
+          source={source}
+          title={title}
+          shortUrl={shortUrl}
+          rating={rating}
+        />
+      </View>
     </SafeAreaView>
   );
 }
