02ef2d92a4b34a31ca23f70d8adea9b50fe0ade4
Arman Attarzadeh <3622268+armanatz@users.noreply.github.com>
2023-09-02 17:56:24 +0800

    Create GIPHY API typings

--- @types/giphy.d.ts
+++ @types/giphy.d.ts
@@ -1,1 +1,51 @@
 export type GIFContentRatings = 'g' | 'pg' | 'pg13' | 'r';
+
+export type CommonImageObjectProps = {
+  url: string;
+  width: string;
+  height: string;
+};
+
+export interface OriginalImageObjectProps extends CommonImageObjectProps {
+  webp?: string;
+}
+
+export type ImagesObject = {
+  downsampled: CommonImageObjectProps;
+  fixed_height_small_still: CommonImageObjectProps;
+  original: OriginalImageObjectProps;
+};
+
+export type GIFObject = {
+  id: string;
+  url: string;
+  bitly_url: string;
+  rating: GIFContentRatings;
+  images: ImagesObject;
+  title: string;
+};
+
+export type MetaObject = {
+  msg: string;
+  status: number;
+  response_id: string;
+};
+
+export type PaginationObject = {
+  offset: number;
+  total_count: number;
+  count: number;
+};
+
+interface BaseAPIResponse {
+  meta: MetaObject;
+}
+
+export interface SingleGIFAPIResponse extends BaseAPIResponse {
+  data: GIFObject;
+}
+
+export interface MultiGIFAPIResponse extends BaseAPIResponse {
+  data: GIFObject[];
+  pagination: PaginationObject;
+}
