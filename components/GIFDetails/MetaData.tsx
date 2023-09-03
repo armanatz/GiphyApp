a1d894189c409abd99ed024159064e71ebb58d66
Arman Attarzadeh <3622268+armanatz@users.noreply.github.com>
2023-09-03 14:51:49 +0800

    Truncate title and URL text in GIFDetails component

--- components/GIFDetails/MetaData.tsx
+++ components/GIFDetails/MetaData.tsx
@@ -1,5 +1,6 @@
 import { Text, TouchableHighlight, View } from 'react-native';
 import type { ViewProps } from 'react-native';
+import { truncateText } from '../../utils';
 
 type MetaDataProps = {
   title?: string;
@@ -12,10 +13,26 @@ export default function MetaData({
   url,
   containerProps,
 }: MetaDataProps) {
+  function renderTitle() {
+    if (!title) {
+      return 'GIF has no title';
+    }
+
+    return title.length >= 30 ? truncateText(title, 30) : title;
+  }
+
+  function renderUrl() {
+    if (!url) {
+      return 'GIF has no URL';
+    }
+
+    return url.length >= 30 ? truncateText(url, 30) : url;
+  }
+
   return (
-    <View>
-      <Text>{title || 'GIF title not found'}</Text>
-      <Text>{url || 'https://giphy.com/'}</Text>
+    <View {...containerProps}>
+      <Text>{renderTitle()}</Text>
+      <Text>{renderUrl()}</Text>
     </View>
   );
 }
