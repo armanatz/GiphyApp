f23399ba7d72f6a7efe83d7149c87b86d6e9cde6
Arman Attarzadeh <3622268+armanatz@users.noreply.github.com>
2023-09-05 00:18:43 +0800

    Move process.env variable into hook function
    Defining the expo process.env variables outside of the function scope in query hooks causes them to be undefined when tests are run inside Jest environment

--- hooks/queries/useGIFSearch.ts
+++ hooks/queries/useGIFSearch.ts
@@ -4,11 +4,6 @@ import type { MultiGIFAPIResponse } from '../../@types/giphy';
 
 const SEARCH_GIF_ENDPOINT = '/gifs/search';
 
-const {
-  EXPO_PUBLIC_GIPHY_API_URL: GIPHY_API_URL,
-  EXPO_PUBLIC_GIPHY_API_KEY: GIPHY_API_KEY,
-} = process.env;
-
 export default function useGIFSearch(
   { searchQuery = '', offset = 0, limit = 25 },
   useQueryOptions: Omit<
@@ -16,9 +11,13 @@ export default function useGIFSearch(
     'queryKey' | 'queryFn'
   > = {},
 ) {
+  const {
+    EXPO_PUBLIC_GIPHY_API_URL: GIPHY_API_URL,
+    EXPO_PUBLIC_GIPHY_API_KEY: GIPHY_API_KEY,
+  } = process.env;
+
   async function searchGIF() {
     const queryParamList = [
-      `?api_key=${GIPHY_API_KEY}`,
       `&q=${searchQuery}`,
       `&limit=${limit}`,
       `&offset=${offset}`,
@@ -26,12 +25,19 @@ export default function useGIFSearch(
 
     try {
       const req = await fetch(
-        `${GIPHY_API_URL}${SEARCH_GIF_ENDPOINT}${queryParamList.join('')}`,
+        `${GIPHY_API_URL}${SEARCH_GIF_ENDPOINT}?api_key=${GIPHY_API_KEY}${queryParamList.join(
+          '',
+        )}`,
       );
+
+      if (!req.ok) {
+        throw new Error('GIF search failed. Try again.');
+      }
+
       const data = await req.json();
       return data;
     } catch (err) {
-      return err;
+      throw err;
     }
   }
 
--- hooks/queries/useRandomGIF.ts
+++ hooks/queries/useRandomGIF.ts
@@ -4,26 +4,31 @@ import type { SingleGIFAPIResponse } from '../../@types/giphy';
 
 const RANDOM_GIF_ENDPOINT = '/gifs/random';
 
-const {
-  EXPO_PUBLIC_GIPHY_API_URL: GIPHY_API_URL,
-  EXPO_PUBLIC_GIPHY_API_KEY: GIPHY_API_KEY,
-} = process.env;
-
 export default function useRandomGIF(
   useQueryOptions: Omit<
     UseQueryOptions<SingleGIFAPIResponse, unknown, SingleGIFAPIResponse>,
     'queryKey' | 'queryFn'
   > = {},
 ) {
+  const {
+    EXPO_PUBLIC_GIPHY_API_URL: GIPHY_API_URL,
+    EXPO_PUBLIC_GIPHY_API_KEY: GIPHY_API_KEY,
+  } = process.env;
+
   async function getRandomGIF() {
     try {
       const req = await fetch(
         `${GIPHY_API_URL}${RANDOM_GIF_ENDPOINT}?api_key=${GIPHY_API_KEY}`,
       );
+
+      if (!req.ok) {
+        throw new Error('Getting random GIF failed. Try again.');
+      }
+
       const data = await req.json();
       return data;
     } catch (err) {
-      return err;
+      throw err;
     }
   }
 
